e3e494ed59fb958772a512d2d813f9a1f4cb701c
caffgeek <gibble@gmail.com>
2019-01-05 09:59:07 -0600

    286.7

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -549,7 +549,7 @@
   #define DELTA_DIAGONAL_ROD 267.0 // mm
 
   // height from z=0 to home position
-  #define DELTA_HEIGHT 286.15 // get this value from auto calibrate
+  #define DELTA_HEIGHT 286.70 // get this value from auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // get these from auto calibrate
 
